c594615b3c6b890bba209ec12989cbe71e6bfff3
Ireaurora <ireaurora@gmail.com>
2020-08-14 08:36:18 +0100

    Created repo

---
+++ CatFacts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatFacts
+//
+//  Created by Irene Sarigu on 14/08/2020.
+//  Copyright © 2020 Irene Sarigu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
